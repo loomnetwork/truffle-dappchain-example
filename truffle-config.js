67804cc7da29554c19cbd3cc28bc8d1641c29142
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2018-06-12 09:26:22 -0300

    Adding truffle box json

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,5 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  // or just copy the configuration on truffle.js here
 };
