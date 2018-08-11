1ee7ea71f90461a6dedcfdbbc0a6dd6c62040ad4
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2018-08-11 16:14:51 -0300

    Move truffle.js to truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,21 @@
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const LoomTruffleProvider = require('loom-truffle-provider')
+
+const chainId = 'default'
+const writeUrl = 'http://127.0.0.1:46658/rpc'
+const readUrl = 'http://127.0.0.1:46658/query'
+const privateKey = readFileSync('./private_key', 'utf-8')
+
+const loomTruffleProvider = new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey)
+loomTruffleProvider.createExtraAccounts(10)
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  // or just copy the configuration on truffle.js here
-};
+  contracts_build_directory: join(__dirname, './src/contracts'),
+  networks: {
+    loom_dapp_chain: {
+      provider: loomTruffleProvider,
+      network_id: '*'
+    }
+  }
+}
