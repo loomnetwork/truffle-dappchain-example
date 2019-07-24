5d68094eba654f386e5562e15ed6ef0daa1ccc9b
Deeprom S <grooveeeeee@gmail.com>
2019-07-24 17:51:54 +0700

    add comment and prevent blank variable to deploy

--- migrations/5_deploy_sampleBEP2Token_contracts.js
+++ migrations/5_deploy_sampleBEP2Token_contracts.js
@@ -1,12 +1,13 @@
 // This is an example for how to deploy SampleBEP2Token
 var SampleBEP2Token = artifacts.require("SampleBEP2Token");
 
-let gatewayAddress = ""
+let gatewayAddress = "" // dappchain gateway address according to deploying network
 
-let name = ""
+let name = "" // token name
 
-let symbol = ""
+let symbol = "" // token symbol
 
 module.exports = function(deployer) {
+  if (gatewayAddress === "" || name === "" || symbol === "") return
   deployer.deploy(SampleBEP2Token, gatewayAddress, name, symbol);
 };
